a3d9e8621ad9f4b4114b353cac53b329239f161c
Glenn 'devalias' Grant <glenn@ensquared.net>
2016-11-01 13:57:31 +1100

    Fix https://github.com/SpiderLabs/ikeforce/issues/2

--- ikeclient.py
+++ ikeclient.py
@@ -216,7 +216,7 @@ class IKEv1Client(object):
 		lifeDur = "000c0004" + str(ld) # need to handle this better
 
         	try:
-        	        strPayload = payNext+padding+transNum+transID+padding1+encTypehashType+authtype+groupType+lifeType+lifeDur+keyLen
+        	        strPayload = payNext+padding+transNum+transID+padding1+encType+hashType+authType+groupType+lifeType+lifeDur+keyLen
         	except:
         	        strPayload = payNext+padding+transNum+transID+padding1+encType+hashType+authType+groupType+lifeType+lifeDur
 
